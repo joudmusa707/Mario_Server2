1715e1f70d24d0824fbd31d0d3b115e7f93c6d2d
joudmusa707 <ID+username@users.noreply.github.com>
2026-06-09 15:07:50 +0300

    final adits

--- server.js
+++ server.js
@@ -39,5 +39,7 @@ server.use((req, res) => {
 });
 
 db.connect().then(() => {
-  server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+  server.listen(PORT, "0.0.0.0", () =>
+    console.log(`Server is running on port ${PORT}`),
+  );
 });
